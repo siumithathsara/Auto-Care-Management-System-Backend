186a92ace7d0cfbb343cb9ff87415423d16f3741
imakshi siyumi <sithuthathsara@gmail.com>
2026-08-09 19:00:31 +0530

    first commit

---
+++ AutoCareManagement/src/main/java/ijse/lk/AutoCareManagement/AutoCareManagementApplication.java
@@ -0,0 +1,13 @@
+package ijse.lk.AutoCareManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCareManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCareManagementApplication.class, args);
+	}
+
+}
