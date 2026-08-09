cdc0f33b8b52dd057d2ceaa86fbf59befc805705
imakshi siyumi <sithuthathsara@gmail.com>
2026-08-09 19:18:55 +0530

    add response classes and compiler configuration for AutoCareManagement

---
+++ AutoCareManagement/src/main/java/ijse/lk/AutoCareManagement/constant/ResponseMessage.java
@@ -0,0 +1,7 @@
+package ijse.lk.AutoCareManagement.constant;
+
+public class ResponseMessage {
+
+    public static String SUCCESS_MESSAGE = "Operation Successful";
+    public static String FAILURE_MESSAGE = "Operation Failed";
+}
---
+++ AutoCareManagement/src/main/java/ijse/lk/AutoCareManagement/constant/ResponseStatusCode.java
@@ -0,0 +1,7 @@
+package ijse.lk.AutoCareManagement.constant;
+
+public class ResponseStatusCode {
+
+    public static  int OPERATION_SUCCESS = 0;
+    public static  int OPERATION_FAILURE = 1;
+}
